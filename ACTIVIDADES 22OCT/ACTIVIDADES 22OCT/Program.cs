0e8287be13a28ffad67bc29ad7da020332824690
aldoNFierr05 <adnr01@hotmail.com>
2019-10-22 09:52:35 -0700

    estos son arreglos en c sharp

---
+++ ACTIVIDADES 22OCT/ACTIVIDADES 22OCT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTIVIDADES_22OCT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("elije una opción");
+            a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    arreglos.númerosAscendentes();
+                    break;
+                case 2:
+
+                    arreglos.númerosDescendentes();
+                    break;
+
+
+
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ ACTIVIDADES 22OCT/ACTIVIDADES 22OCT/arreglos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTIVIDADES_22OCT
+{
+    class arreglos
+    {
+        public static void númerosAscendentes()
+        {
+            Console.WriteLine("los números al 100 son: /n");
+            int[] arreglo = new int[1];
+            for (int i = 0; i < 100; i++)
+
+                Console.WriteLine(i + 1);
+
+        }
+
+        public static void númerosDescendentes()
+        {
+            Console.WriteLine("los números del 100 al 1");
+            int[] arreglo = new int[1];
+            for (int i = 0; 0 < 100; i--)
+
+                Console.WriteLine(i + 1);
+
+        }
+    }
+}
